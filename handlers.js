6410fff97407e5af4dff4c0efe48f3349b91d579
BABITA-05 <bhandaribabita383@gmail.com>
2026-05-13 09:59:41 +0545

    day2

--- handlers.js
+++ handlers.js
@@ -2,7 +2,26 @@ let initialRouteHandler = (req, res)=>{
     res.send("My first api is successful")
 }
 let getAllUsersHandler = (req, res)=>{
-    console.log("/users called with get method")
+    console.log("users called with get method")
     res.send("all users fetched")
 }
-export {initialRouteHandler, getAllUsersHandler}
+let handlerCreateUser = (req, res)=>{
+        let reqData = req.body
+
+        let data = {
+            message:"i have received the following data",
+            data:reqData
+        }
+        res.json(data)
+    }
+
+let handlerCalculate = (req, res)=>{
+       let reqData = req.body
+
+       let data = {
+        message:"we have following data",
+        data:reqData
+       }
+       res.json(data)
+    }
+export {initialRouteHandler, getAllUsersHandler, handlerCreateUser, handlerCalculate}
--- index.js
+++ index.js
@@ -3,12 +3,26 @@
 
 // const app = express();
 import express from "express";
-import { initialRouteHandler, getAllUsersHandler } from "./handlers.js";
+import { initialRouteHandler, getAllUsersHandler, handlerCreateUser, handlerCalculate } from "./handlers.js";
 let app = express()
 
+//middleware for body parse so the json body gets converted
+app.use(express.json())
+
+//creating route
 app.get("/", initialRouteHandler);
 app.get("/users", getAllUsersHandler)
 
+app.post(
+    "/user/create", handlerCreateUser
+)
+
+app.post(
+    "/calculate", handlerCalculate
+)
+
+
+//creating server
 app.listen(8080, ()=>{
     console.log("server started on port 8080")
 });
