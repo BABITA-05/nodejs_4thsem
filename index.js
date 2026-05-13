423253d447f5f6ee2b8475d7b3382708dac61084
BABITA-05 <bhandaribabita383@gmail.com>
2026-05-13 22:40:48 +0545

    day3

--- index.js
+++ index.js
@@ -3,11 +3,18 @@
 
 // const app = express();
 import express from "express";
+import dotenv from "dotenv"
 import { initialRouteHandler, getAllUsersHandler, handlerCreateUser, handlerCalculate } from "./handlers.js";
+
+//loading .env
+dotenv.config()
 let app = express()
 
 //middleware for body parse so the json body gets converted
 app.use(express.json())
+
+//getting port from env
+let PORT = process.env.PORT
 
 //creating route
 app.get("/", initialRouteHandler);
@@ -19,6 +26,21 @@ app.post(
 
 app.post(
     "/calculate", handlerCalculate
+)
+
+app.get(
+    "/multiply", (req, res)=>{
+        let data = req.body
+
+        let a = data.a
+        let b = data.b
+        let result = a * b
+        let resData = {
+            "message":"multiplied successfully",
+            data: result
+        }
+        res.status(200).json(resData)
+    }
 )
 
 
